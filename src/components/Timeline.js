fc0ae9c6d847aaedd8baa727a4ed93c64e2ece9a
Alice Boone <alicegbonel@gmail.com>
2020-12-21 14:48:03 -0800

    Timeline implementation

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,26 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
+const Timeline = (props) => {
+
+  const eventComponents = props.events.map((event, i) => {
+    return(
+      <div className='event-timeline'>
+        <TimelineEvent
+          key={i}
+          person={event.person}
+          status={event.status}
+          timeStamp={event.timeStamp}
+        />
+      </div>
+    );
+  });
+
+  return (
+    <div className='timeline'>
+      {eventComponents}
+    </div>
+  );
 }
 
 export default Timeline;
